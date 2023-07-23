eb788a5baa3d7a919cb8f0500558dccea03856bd
Ihor Arkushyn <114363817+IhorArkushyn@users.noreply.github.com>
2023-07-23 13:25:12 +0300

    Initial commit

---
+++ src/components/ContactList/ContactList.styled.jsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const List = styled.ul`
+  display: flex;
+  flex-direction: column;
+  padding: 0 15px;
+  width: 360px;
+`;
+
+export const Text = styled.p`
+  padding: 0 15px;
+`;
